260e8be28501d3fda08e5dd0f651743d2fc77290
AslanGhagigat <aslan1382ss@gmail.com>
2023-03-14 00:48:35 +0330

    delete additional codes

--- components/password/Password.jsx
+++ components/password/Password.jsx
@@ -13,21 +13,5 @@ export default function Password(props) {
         <AiOutlineEye className="pointer-events-auto w-auto h-8 absolute top-1/2 transform -translate-y-1/2 right-4"/>
       </label>
     </div>
-    // <div className="h-auto w-full pr-2 flex flex-row justify-center items-center text-white">
-    //   {props.children}
-    //   <div className="w-full h-10 border-2 border-white rounded-lg flex justify-center items-center ">
-    //     <input
-    //       className="h-full w-full px-2 bg-black rounded-md placeholder-white focus:border-black"
-    //       type="password"
-    //       placeholder="password"
-    //     />
-    //     <button className="absolute right-9">
-    //       <AiOutlineEye
-    //         className="w-auto h-8 text-white m-1 rounded-md "
-    //         size={25}
-    //       />
-    //     </button>
-    //   </div>
-    // </div>
   );
 }
